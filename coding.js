4f3c49f85b4ac48dc2c2e0468edbec7ca83ab347
ol1oly <olivier.girardville@outlook.com>
2024-10-17 02:31:44 -0400

    updated CV

--- coding.js
+++ coding.js
@@ -1,13 +1,13 @@
-var row1 = document.getElementById("row1");
+/*var row1 = document.getElementById("row1");
 var row2 = document.getElementById("row2");
 var row3 = document.getElementById("row3");
 var row4 = document.getElementById("row4");
 
-var currentImage = document.getElementById("contentImg");
 
-const rows = [row1, row2, row3,row4];
-var index=0;
-const images=["images/anthony.jpg","images/king.jpg","images/ches.png","images/mbappe.jpg","images/java.png","images/skeletons.jpg"];
+
+const rows = [row1, row2, row3];
+
+
 /*Solution: make three images, 1 active, 1 left and 1 right. when arrow pressed, the correct image slides into the middle, while the middle one
             goes to be outside view. After transition, middle changes its src to be the new one, while right and left changes their src to be 
             correct with the middle one. 
@@ -17,30 +17,49 @@ const images=["images/anthony.jpg","images/king.jpg","images/ches.png","images/m
 /*  MAYBE, include blender
 To put in content: aimlab, chess
                     calculator, weather app, quizapp, logo detection
-                    traffic, mot cachés, caveVin
+                    traffic, mot cachés
                     simulation motor
 */
 
 function goTo(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
-
+var currentImage = document.getElementById("contentImg");
+var desc = document.getElementById("desc");
+const images=["images/Aimlab.png","images/chessGame.png","images/guessTemp.png","images/rushHour.png","images/mots.png","images/countryQuiz.png"];
+const description = [
+    "Made in Unity", 
+    "Made in Unity, fully functional chess game. Plans to make it multiplayer",
+    "Made with Python and Tkinter, using OpenWeatherMap API",
+    "Made in school with JavaFX, recreation of the game Rush Hour",
+    "Made in school with JavaFX, recreation of the Crossword game",
+    "Made with Tkinter, using GeoNames API, an API with geographic data"
+];
+var index=0;
+/*
 function changeColor(item){
     for (let i = 0; i < rows.length; i++) {
         rows[i].style.backgroundColor="white";
+        rows[i].style.color="black";
+        
       }
-      item.style.backgroundColor="yellow";
+      item.style.backgroundColor="#0066ff";
+      item.style.color="white";
+
+
       index=0;
       if(item == rows[0])  currentImage.src = "images/java.png";
       if(item == rows[1])  currentImage.src = "images/python.webp";
       if(item == rows[2])  currentImage.src = "images/uni.webp";
-      if(item == rows[3])  currentImage.src = "images/godo.png";
-}
+      
+}*/
 
 
 function arrowClick(change){
     index+=change;
+    if (index<0) index=images.length-1;
     currentImage.src = images[index % images.length];
+    desc.innerHTML= description[index % images.length];
 }
 
 
@@ -54,18 +73,20 @@ document.getElementById("buttonProjects").addEventListener('click', function() {
 });
 
 document.getElementById("left-arrow").addEventListener('click', function() {
-    arrowClick(1);
-});
-document.getElementById("right-arrow").addEventListener('click', function() {
     arrowClick(-1);
 });
+document.getElementById("right-arrow").addEventListener('click', function() {
+    arrowClick(1);
+});
 
 
+/*
 for (let i = 0; i < rows.length; i++) {
     rows[i].addEventListener('click', function() {
         changeColor(rows[i]);
     });
-  }
+    
+  }*/
 
 
 
