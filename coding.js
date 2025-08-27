b785d1e88fbbf3d192c2132a24a6dbc939aa669d
ol1oly <olivier.girardville@outlook.com>
2025-08-26 22:12:16 -0400

    updating design
    adding three new skills, upgrading the arrows of the  carousel to be carets icons, as well as adding new hover effects and updating the description

--- coding.js
+++ coding.js
@@ -1,25 +1,3 @@
-/*var row1 = document.getElementById("row1");
-var row2 = document.getElementById("row2");
-var row3 = document.getElementById("row3");
-var row4 = document.getElementById("row4");
-
-
-
-const rows = [row1, row2, row3];
-
-
-/*Solution: make three images, 1 active, 1 left and 1 right. when arrow pressed, the correct image slides into the middle, while the middle one
-            goes to be outside view. After transition, middle changes its src to be the new one, while right and left changes their src to be 
-            correct with the middle one. 
-
-            might have to change the language selector to be on top, rather than on the left
-*/
-/*  MAYBE, include blender
-To put in content: aimlab, chess
-                    calculator, weather app, quizapp, logo detection
-                    traffic, mot cachés
-                    simulation motor
-*/
 
 function goTo(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
