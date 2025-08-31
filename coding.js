7a5fb2853e12bd9476859a3ee2495e1fd6152f1a
ol1oly <olivier.girardville@outlook.com>
2025-08-31 17:29:17 -0400

    aesthetic changes
    changed the colors, and added variables for them. Remove unused properties and made the css cleaner

--- coding.js
+++ coding.js
@@ -1,4 +1,17 @@
 
+/*Solution: make three images, 1 active, 1 left and 1 right. when arrow pressed, the correct image slides into the middle, while the middle one
+            goes to be outside view. After transition, middle changes its src to be the new one, while right and left changes their src to be 
+            correct with the middle one. 
+
+            might have to change the language selector to be on top, rather than on the left
+*/
+/*  MAYBE, include blender
+To put in content: aimlab, chess
+                    calculator, weather app, quizapp, logo detection
+                    traffic, mot cachés
+                    simulation motor
+*/
+
 function goTo(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
@@ -10,8 +23,8 @@ const description = [
     "Made in Unity, fully functional chess game. Plans to make it multiplayer",
     "Made with Python and Tkinter, using OpenWeatherMap API. Guess the temperature of a city",
     "Made with Tkinter, using GeoNames API, an API with geographic data. Quiz about countries, cities and capitals.",
-    "Made in school with JavaFX, recreation of the game Rush Hour",
-    "Made in school with JavaFX, recreation of the Crossword game"
+    "Made in school with JavaFX, recreation of the Crossword game",
+    "Made in school with JavaFX, recreation of the game Rush Hour"
 ];
 var index = 0;
 /*
@@ -56,15 +69,6 @@ document.getElementById("left-arrow").addEventListener('click', function () {
 document.getElementById("right-arrow").addEventListener('click', function () {
     arrowClick(1);
 });
-
-
-/*
-for (let i = 0; i < rows.length; i++) {
-    rows[i].addEventListener('click', function() {
-        changeColor(rows[i]);
-    });
-    
-  }*/
 
 
 
